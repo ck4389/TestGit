0784e6a9b0eaf23fac661884851e4760e2d1185e
ck <1227533927@qq.com>
2022-05-12 19:44:46 +0800

    第三次修改

--- src/main/java/com/xxxx/App.java
+++ src/main/java/com/xxxx/App.java
@@ -10,10 +10,5 @@ public class App
     {
         System.out.println( "Hello World!1" );
         System.out.println( "Hello World!2" );
-        System.out.println( "Hello World!3" );
-        System.out.println( "Hello World!3" );
-        System.out.println( "Hello World!dev" );
-        System.out.println( "Hello World!dev2" );
-        System.out.println( "Hello World!dev3" );
     }
 }
